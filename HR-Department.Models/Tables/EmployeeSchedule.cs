4cadf71490aab231f8abefba3148203687572679
Jonathan Guinto <xianlee.ivan.guinto@gmail.com>
2016-03-28 13:04:30 +0800

    add EmployeeSchedule Table

---
+++ HR-Department.Models/Tables/EmployeeSchedule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DapperExtensions.Mapper;
+
+namespace HR_Department.Models.Tables
+{
+    public class EmployeeSchedule
+    {
+        public int EmployeeScheduleId { get; set; }
+        public int EmployeeId { get; set; }
+        public int ScheduleId { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+
+    public sealed class EmployeScheduleMapper : ClassMapper<EmployeeSchedule>
+    {
+        public EmployeScheduleMapper()
+        {
+            Table("EmployeeSchedule");
+            Map(s => s.EmployeeScheduleId).Key(KeyType.Identity);
+            AutoMap();
+        }
+    }
+}
