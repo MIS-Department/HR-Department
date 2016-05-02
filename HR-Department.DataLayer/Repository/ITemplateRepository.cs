0277999e6ffbd332571fed774c2ba606bba680e7
snuad <snuad07@gmail.com>
2016-05-02 19:33:15 +0800

    Template

---
+++ HR-Department.DataLayer/Repository/ITemplateRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HR_Department.Models.Tables;
+
+namespace HR_Department.DataLayer.Repository
+{
+    public interface ITemplateRepository
+    {
+        Task<IEnumerable<Template>> SelectAll();
+        Task<Template> SelectById(int? id);
+        Task<int> Insert(Template model);
+        //Task Delete(int? id);
+        //Task Update(Template model);
+        //Task<IEnumerable<Template>> SelectByName(string name);
+    }
+}
